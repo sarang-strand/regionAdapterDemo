faf28c0b8142cbd16627c41f5fe4136f25363b90
Sarang <sarang.bhutada@strandls.com>
2020-08-25 14:45:52 +0530

    First commit.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Prism.Ioc;
+using Prism.Regions;
+using System.Windows;
+
+namespace demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IContainerExtension _container;
+        IRegionManager _regionManager;
+
+        public MainWindow(IContainerExtension container, IRegionManager regionManager)
+        {
+            InitializeComponent();
+            _container = container;
+            _regionManager = regionManager;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //var view = _container.Resolve<ViewA>();
+            IRegion region = _regionManager.Regions["ContentRegion"];
+            region.Add(_container.Resolve<UserControl1>());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            IRegion region = _regionManager.Regions["ContentRegion"];
+            region.RemoveAll();
+        }
+    }
+}
---
+++ StackRegionAdapter.cs
@@ -0,0 +1,44 @@
+﻿using Prism.Regions;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace demo
+{
+    class StackRegionAdapter : RegionAdapterBase<StackPanel>
+    {
+        public StackRegionAdapter(IRegionBehaviorFactory regionBehaviorFactory)
+            : base(regionBehaviorFactory)
+        {
+
+        }
+
+        protected override void Adapt(IRegion region, StackPanel regionTarget)
+        {
+            region.Views.CollectionChanged += (s, e) =>
+            {
+                if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+                {
+                    //MessageBox.Show("Adapt is called for adding views!!");
+                    foreach (FrameworkElement element in e.NewItems)
+                    {
+                        regionTarget.Children.Add(element);
+                    }
+                }
+
+                else if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+                {
+                    int i = region.Views.Count();
+                    string msg = "Removing view number {0}!!";
+                    MessageBox.Show(string.Format(msg, region.Views.Count()));
+                    regionTarget.Children.RemoveAt(regionTarget.Children.Count - 1);
+                }
+            };
+        }
+
+        protected override IRegion CreateRegion()
+        {
+            return new Region();
+        }
+    }
+}
